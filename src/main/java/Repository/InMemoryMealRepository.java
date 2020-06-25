169e2524b2f08256cf7716d8af9505bede29a8d6
Dima <dm.golubckov@mail.ru>
2020-06-25 17:35:29 +0300

    add filter by section

--- src/main/java/Repository/InMemoryMealRepository.java
+++ src/main/java/Repository/InMemoryMealRepository.java
@@ -4,9 +4,11 @@ import model.Meal;
 import parser.MealParser;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
     private Map<Integer, Meal> mealMap = new ConcurrentHashMap<>();
@@ -34,6 +36,9 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Collection<Meal> getAll() {
+        /*return mealMap.isEmpty() ? parser.getAllMeal() : mealMap.values().stream()
+                .sorted(Comparator.comparing(Meal::getCaloriesPerServing).reversed())
+                .collect(Collectors.toList());*/
         return mealMap.isEmpty() ? parser.getAllMeal() : mealMap.values();
     }
 }
